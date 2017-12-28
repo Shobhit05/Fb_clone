fcaa8476c9acdb746fd14c12056c0fe46db24333
shobhit05 <shobhit.srivastava39@gmail.com>
2017-12-28 02:59:10 -0800

    New Changes

--- django2/clone/clone/urls.py
+++ django2/clone/clone/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls import url ,include
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('fb.urls')),
